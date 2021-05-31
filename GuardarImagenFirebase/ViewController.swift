e1f7e3adeb4c91209f895dcd567493f3d51f871f
marcoalonso <marcoalonsorota@hotmail.com>
2021-05-31 10:45:32 -0500

    GestureRecognizer a imagenSubir

--- GuardarImagenFirebase/ViewController.swift
+++ GuardarImagenFirebase/ViewController.swift
@@ -11,9 +11,28 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imagenASubir: UIImageView!
     @IBOutlet weak var imagenADescargar: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //MARK:-Agregar gestura a la imagen
+        
+        let gestura = UITapGestureRecognizer(target: self, action: #selector(clickImagen))
+        
+        gestura.numberOfTapsRequired = 1
+        gestura.numberOfTouchesRequired = 1
+         //agregar la gestura a la imagen
+        imagenASubir.addGestureRecognizer(gestura)
+        imagenASubir.isUserInteractionEnabled = true
+    }
+    
+    @objc func clickImagen(gestura: UITapGestureRecognizer) {
+        print("Cambiar imagen")
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true, completion: nil)
     }
 
     @IBAction func subirFotoBtn(_ sender: UIButton) {
@@ -23,3 +42,16 @@ class ViewController: UIViewController {
     
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imagenSeleccionada = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
+            imagenASubir.image = imagenSeleccionada
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
