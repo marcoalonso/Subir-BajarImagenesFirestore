9a110d4b410bfd4aa5e32fca72e56385030818b0
marcoalonso <marcoalonsorota@hotmail.com>
2021-05-31 10:42:08 -0500

    UI creada

--- GuardarImagenFirebase/ViewController.swift
+++ GuardarImagenFirebase/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imagenASubir: UIImageView!
+    @IBOutlet weak var imagenADescargar: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func subirFotoBtn(_ sender: UIButton) {
+    }
+    @IBAction func DescargarFotoBtn(_ sender: UIButton) {
+    }
+    
 }
 
